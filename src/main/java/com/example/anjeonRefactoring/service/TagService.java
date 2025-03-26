718de5d22227062c3ddce8fc35ae1abc87b1e4cf
임태연 <limtaeyeon@imtaeyeon-ui-MacBookAir.local>
2025-03-26 18:20:41 +0900

    show/tag

--- src/main/java/com/example/anjeonRefactoring/service/TagService.java
+++ src/main/java/com/example/anjeonRefactoring/service/TagService.java
@@ -3,9 +3,14 @@ package com.example.anjeonRefactoring.service;
 import com.example.anjeonRefactoring.domain.Tag;
 import com.example.anjeonRefactoring.repository.TagRepository;
 import com.example.anjeonRefactoring.repuestDto.CreateTagDto;
+import com.example.anjeonRefactoring.responsDto.ShowTagDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +28,30 @@ public class TagService {
         tagRepository.save(tag);
 
         return tag;
+    }
+
+    public List<ShowTagDto> showTagAll() {
+
+        Map<String, List<Tag>> groupedByCategory = tagRepository.findAll()
+                .stream()
+                .collect(Collectors.groupingBy(Tag::getCategory));
+
+        return groupedByCategory.entrySet()
+                .stream()
+                .map(entry -> {
+                    String category = entry.getKey();
+
+                    // tagName과 tagId를 Map으로 묶음
+        Map<Long, String> tagMap = entry.getValue()
+                .stream()
+                .collect(Collectors.toMap(Tag::getId, Tag::getTagName));
+
+        return ShowTagDto.builder()
+                .tagMap(tagMap) // tagMap을 저장
+                .category(category)
+                .build();
+                })
+                .collect(Collectors.toList());
+
     }
 }
